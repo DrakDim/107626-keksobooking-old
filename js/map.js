54d82638253e3032d01050142d6f7d74dc8ce292
DrakDim <darsten.d@gmail.com>
2017-12-10 22:01:57 +0300

    Выполнил часть правок согласно некоторым замечаниям

--- js/map.js
+++ js/map.js
@@ -1,9 +1,5 @@
 'use strict';
 
-/**
- * Константы
- */
-
 var PRICE_MIN = 1000;
 var PRICE_MAX = 1000000;
 
@@ -12,45 +8,45 @@ var LOCATION_X_MAX = 900;
 var LOCATION_Y_MIN = 100;
 var LOCATION_Y_MAX = 500;
 
-/**
- * Исходные значения
- */
+var MAX_ROOMS = 5;
+var MAX_GUESTS = 100;
 
-var titles = ['Большая уютная квартира',
-  'Маленькая неуютная квартира',
-  'Огромный прекрасный дворец',
-  'Маленький ужасный дворец',
-  'Красивый гостевой домик',
-  'Некрасивый негостеприимный домик',
-  'Уютное бунгало далеко от моря',
-  'Неуютное бунгало по колено в воде'];
-
-var types = ['flat', 'house', 'bungalo'];
-var checks = ['12:00', '13:00', '14:00'];
-var features = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-
-/**
- * Промежуточные вычисления
- */
+var HOME_TITLES = ['Большая уютная квартира', 'Маленькая неуютная квартира', 'Огромный прекрасный дворец',
+  'Маленький ужасный дворец', 'Красивый гостевой домик', 'Некрасивый негостеприимный домик',
+  'Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде'];
+var HOME_TYPES = ['flat', 'house', 'bungalo'];
+var TIME_CHECKS = ['12:00', '13:00', '14:00'];
+var FACILITIES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 
 var getRandomInt = function (min, max)	{
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-var getFeatures = function () {
-  var anyNum = Math.floor(Math.random() * 10);
+var generateNumberImg = function () {
+  /* 'цифры от 1 до 8 повторятся не должны' */
   var anyArrs = [];
-  for (var i = 0; i < anyNum; i++) {
-    anyArrs[i] = features[getRandomInt(0, features.length - 1)];
+  for (var i = 0; i < 8; i++) {
+    anyArrs[i] = i++;
   }
   return anyArrs;
 };
 
-/**
- * Основной объект
- */
+var generateTitles = function () {
+  var oneTitle = HOME_TITLES[getRandomInt(0, HOME_TITLES.length - 1)];
+  /* 'заголовки повторяться не должны' */
+  return oneTitle;
+};
 
-var anyObject = function () {
+var getRandomFacilities = function () {
+  var randomNumber = getRandomInt(0, 10);
+  var anyArrs = [];
+  for (var i = 0; i < randomNumber; i++) {
+    anyArrs[i] = FACILITIES[getRandomInt(0, FACILITIES.length - 1)];
+  }
+  return anyArrs;
+};
+
+var generateObjectForArray = function () {
   var locations = {
     x: getRandomInt(LOCATION_X_MIN, LOCATION_X_MAX),
     y: getRandomInt(LOCATION_Y_MIN, LOCATION_Y_MAX)
@@ -58,18 +54,18 @@ var anyObject = function () {
   var location = locations.x + ', ' + location.y;
   return {
     author: {
-      avatar: 'img/avatars/user0' + 'цифры от 1 до 8 повторятся не должны' + '.png'
+      avatar: 'img/avatars/user0' + generateNumberImg() + '.png'
     },
     offer: {
-      title: 'заголовки повторяться не должны',
+      title: generateTitles(),
       addres: location,
       price: getRandomInt(PRICE_MIN, PRICE_MAX),
-      type: types[getRandomInt(0, 2)],
-      rooms: getRandomInt(1, 5),
-      guests: getRandomInt(1, 100),
-      checkin: checks[getRandomInt(0, 2)],
-      checkout: checks[getRandomInt(0, 2)],
-      features: getFeatures(),
+      type: HOME_TYPES[getRandomInt(0, HOME_TYPES.length - 1)],
+      rooms: getRandomInt(1, MAX_ROOMS),
+      guests: getRandomInt(1, MAX_GUESTS),
+      checkin: TIME_CHECKS[getRandomInt(0, TIME_CHECKS.length - 1)],
+      checkout: TIME_CHECKS[getRandomInt(0, TIME_CHECKS.length - 1)],
+      features: getRandomFacilities(),
       description: '',
       photos: []
     },
@@ -79,3 +75,5 @@ var anyObject = function () {
     }
   };
 };
+
+generateObjectForArray();
