16c7c8fa079bffb0489ea39ce182f6ecd5fd6277
DrakDim <darsten.d@gmail.com>
2017-12-12 19:44:00 +0300

    Создавал предварительный код для выполнения задания

--- js/map.js
+++ js/map.js
@@ -15,6 +15,8 @@ var MAX_ROOMS = 5;
 var MIN_GUESTS = 1;
 var MAX_GUESTS = 100;
 
+/* var OFSSET_PiN = 65; */
+
 var HOME_TITLES = ['Большая уютная квартира', 'Маленькая неуютная квартира', 'Огромный прекрасный дворец',
   'Маленький ужасный дворец', 'Красивый гостевой домик', 'Некрасивый негостеприимный домик',
   'Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде'];
@@ -71,3 +73,44 @@ for (var i = 0; i < ORDER_LIMIT; i++) {
 }
 
 document.querySelector('.map').classList.remove('map--faded');
+
+
+var renderPin = function (order) {
+  var initialPin = document.querySelector('template button.map__pin').cloneNode(true);
+  initialPin.style.left = order.location.x;
+  initialPin.style.top = order.location.y;
+  initialPin.querySelector('img').src = order.author.avatar;
+  return initialPin;
+};
+
+var renderOrder = function (order) {
+  var initialOrder = document.querySelector('template article.map__card').cloneNode(true);
+  initialOrder.querySelector('h3').textContent = order.offer.title;
+  initialOrder.querySelector('p small').textContent = order.offer.address;
+  initialOrder.querySelector('.popup__price').textContent = order.offer.price + '&#x20bd;/ночь';
+  if (order.offer.type === 'flat') {
+    initialOrder.querySelector('h4').textContent = 'Квартира';
+  } else if (order.offer.type === 'bungalo') {
+    initialOrder.querySelector('h4').textContent = 'Бунгало';
+  } else if (order.offer.type === 'house') {
+    initialOrder.querySelector('h4').textContent = 'Дом';
+  } else {
+    initialOrder.querySelector('h4').textContent = 'Некорректное значение';
+  }
+  initialOrder.child[6].textContent = order.offer.rooms + ' для ' + order.offer.guests + ' гостей';
+  initialOrder.child[7].textContent = 'Заезд после ' + order.offer.checkin + ', выезд до ' + order.offer.checkout;
+
+  var oneFeature = initialOrder.querySelector('.popup__features li').cloneNode;
+  for (i = 0; i < order.offer.features.length; i++) {
+    oneFeature.classList.add('feature feature--' + order.offer.features[i]);
+    initialOrder.appendChild(oneFeature);
+  }
+  initialOrder.child[9].textContent = order.offer.description;
+  return initialOrder;
+};
+
+var fragment = document.createDocumentFragment();
+for (i = 0; i < orders.length; i++) {
+  fragment.appendChild(renderPin(orders[i]));
+  fragment.appendChild(renderOrder(orders[i]));
+}
